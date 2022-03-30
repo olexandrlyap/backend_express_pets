e40ed51ccb59df9d28e59a45a4b75fe935bfccd5
MasterMindLegion <40268236+MasterMindLegion@users.noreply.github.com>
2022-03-30 16:53:37 +0200

    Initial commit

---
+++ utils/nodemailerConfig.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+module.exports = {
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+}
+
