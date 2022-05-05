e67aafa8d73a6cc9ee6ba6f3708ff9018b8d78d6
Olexandr <olexandrlyap@gmail.com>
2022-05-05 20:52:26 +0200

    Uninstall unused packages

--- controllers/petUploadImagesController.js
+++ controllers/petUploadImagesController.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const multer  = require('multer');
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const CustomError = require('../errors');
 
 
