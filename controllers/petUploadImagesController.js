2e239fcf8a090388457da17c96cdec349907203e
Olexandr <olexandrlyap@gmail.com>
2022-05-05 20:48:27 +0200

    Remove the unnecessary Multer disk storage

--- controllers/petUploadImagesController.js
+++ controllers/petUploadImagesController.js
@@ -9,17 +9,7 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET
-  });
-
-  // MULTER WITHOUT CLOUDINARY
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/uploads' )
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '--' + file.originalname)
-    }
-}) 
+});
 
 const multerFilter = (req, file, cb) => {
     if(file.mimetype === "image/jpeg" || file.mimetype === "image/jpg" || file.mimetype === "image/png") {
@@ -48,5 +38,4 @@ const imageConfig = upload.fields(([
 
 module.exports = {
     imageConfig,
-
 }
